99a35f4b68c92fdc02a660dde0279270ea4cc1a0
Guillermo Sanchez <guillesafran@gmail.com>
2023-02-06 19:02:35 -0300

    Fetch image id from the first API

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const [imageId, setImageId] = useState('')
+  useEffect(() => {
+    const controller = new AbortController()
+    const signal = controller.signal
+
+    const randomNumber = Math.floor(Math.random() * 100000 + 35000)
+    const API_MUSEUMS = `https://api.artic.edu/api/v1/artworks/${randomNumber}`
+
+    const API_IMAGES = `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`
+    fetch(API_MUSEUMS, { signal })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.data.image_id)
+        setImageId(data)
+      })
+      .catch((err) => {
+        if (err.name === 'AbortError') { console.log('Abort Error') }
+      })
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='App'>
+      hi
     </div>
   )
 }

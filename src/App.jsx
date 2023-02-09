bfdb52c93ae48b61fd15884c7a60c634cfaa9260
Guillermo Sanchez <guillesafran@gmail.com>
2023-02-09 19:07:40 -0300

    Add styling

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,26 @@ function App () {
     )
   } else {
     return (
-      <div className='App'>
-        <ul>
-          <li key={artInformation.title}>{artInformation.title}</li>
-          <li key={artInformation.artist}>{artInformation.artist}</li>
-        </ul>
-        <img key={artInformation.image_id} src={`https://www.artic.edu/iiif/2/${artInformation.image_id}/full/843,/0/default.jpg`} alt={artInformation.title} />
-        <button onClick={() => { setRequestNewArtInfo((prev) => !prev) }}>Get new image</button>
+      <div className='container'>
+        <header><h1>Get a random piece of art from  Art Institute of Chicago</h1></header>
+        <main className='main-container'>
+          <div className='App'>
+            <ul className='art-piece'>
+              <li key={artInformation.title}><h2>{artInformation.title}</h2></li>
+              <li key={artInformation.image_id}>
+                <img
+                  src={`https://www.artic.edu/iiif/2/${artInformation.image_id}/full/843,/0/default.jpg`}
+                  alt={artInformation.title}
+                />
+              </li>
+              <li key={artInformation.artist}>
+                <div>{artInformation.artist}</div>
+              </li>
+            </ul>
+            <button onClick={() => { setRequestNewArtInfo((prev) => !prev) }}>Get new image</button>
+          </div>
+        </main>
+        <footer>Website made by Chicho</footer>
       </div>
     )
   }
